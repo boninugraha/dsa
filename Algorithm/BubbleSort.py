c730f132d50f59d2fdada0163e949f2ab58bdd61
nugrahacode <bonioloff@me.com>
2025-03-18 19:11:16 +0700

    Make the logic inside function

--- Algorithm/BubbleSort.py
+++ Algorithm/BubbleSort.py
@@ -1,11 +1,15 @@
 my_list = [4, 2, 6, 5, 1, 3, 0, 15, 100, -1, 105]
 
-for i in range(len(my_list) - 1):
-    for j in range(len(my_list) - i - 1):
-        # print(f"{my_list[j]} and {my_list[j+1]}")
-        if my_list[j] > my_list[j + 1]:
-            temp = my_list[j]
-            my_list[j] = my_list[j + 1]
-            my_list[j + 1] = temp
 
-print(my_list)
+def bubble_sort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - i - 1):
+            # print(f"{my_list[j]} and {my_list[j+1]}")
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+    return arr
+
+
+print(bubble_sort(my_list))
